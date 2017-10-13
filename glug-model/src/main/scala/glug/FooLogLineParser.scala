d47e4fa82f4a5f013daccfe68397804ca0e36f56
Roberto Tyley <roberto.tyley@theguardian.com>
2017-10-13 10:59:55 +0100

    Fklimp

--- glug-model/src/main/scala/glug/FooLogLineParser.scala
+++ glug-model/src/main/scala/glug/FooLogLineParser.scala
@@ -1,13 +1,14 @@
 package glug
 
 import java.time.{Duration, Instant}
+import java.util.concurrent.TimeUnit
 
 import glug.model.time.{LogInstant, LogInterval}
 import glug.model.{SignificantInterval, ThreadedSystem}
-
 import fastparse.all._
 
 import scala.collection.JavaConverters._
+import scala.concurrent.duration.TimeUnit
 
 
 //2017-10-04 09:08:00,037 [application-akka.actor.default-dispatcher-745] INFO  lib.DashboardData$ - completed lib.DashboardData$::topPagesQueryNonContentAgg in 121 ms elapsed, 116 ms elasticsearch
@@ -19,10 +20,18 @@ import scala.collection.JavaConverters._
 
 case class LogLineStart(instant: Instant, threadName: String, logLevel: String, logger: String)
 
+trait HasElapsedDuration {
+  val elapsedDuration: Duration
+}
+
+case class ESCompleted(query: String, elapsedDuration: Duration, elasticSearchDuration: Duration) extends HasElapsedDuration
+case class AccessCompleted(httpResponseCode: Int, path: String, elapsedDuration: Duration) extends HasElapsedDuration
 
 class FooLogLineParser(threadedSystem: ThreadedSystem) {
 
   val digits: P[Long] = P(CharIn('0' to '9').rep(min = 1) !).map(_.toLong)
+  val intDigits: P[Int] = P(CharIn('0' to '9').rep(min = 1) !).map(_.toInt)
+  val decimal: P[Double] = P((CharIn('0' to '9') | ".").rep(min = 1)!).map(_.toDouble)
 
   val noSpace = CharPred(_ != ' ')
   val space = P(CharsWhileIn(" \r\n").rep(min = 1))
@@ -39,21 +48,51 @@ class FooLogLineParser(threadedSystem: ThreadedSystem) {
 
   val duration: P[Duration] = P(digits ~ " ms").map(Duration.ofMillis)
 
-  val elasticSearchCompletedLine = P(logLineStart ~ "completed " ~ identitifier ~ " in " ~ duration ~ " elapsed, " ~ duration ~ " elasticsearch")
+  object StopwatchTimeParsing {
+    val NANOSECONDS = P("ns").map(_ => TimeUnit.NANOSECONDS)
+
+    val MICROSECONDS = P("\u03bcs").map(_ => TimeUnit.MICROSECONDS) // μs
+
+    val MILLISECONDS = P("ms").map(_ => TimeUnit.MILLISECONDS)
+
+    val SECONDS = P("s").map(_ => TimeUnit.SECONDS)
+
+    val MINUTES = P("min").map(_ => TimeUnit.MINUTES)
+
+    val HOURS = P("h").map(_ => TimeUnit.HOURS)
+
+    val timeUnit: P[TimeUnit] = P(NANOSECONDS | MICROSECONDS | MILLISECONDS | SECONDS | MINUTES | HOURS)
+
+    val stopwatchDuration: P[Duration] = P(decimal ~ " " ~ timeUnit).map { case (num, tu) => Duration.ofNanos(Math.round(num * tu.toNanos(1))) }
+  }
+
+
+  val elasticSearchCompletedLine: P[ESCompleted] = P("completed " ~ identitifier ~ " in " ~ duration ~ " elapsed, " ~ duration ~ " elasticsearch").map(ESCompleted.tupled)
+
+  //2017-10-04 09:08:00,121 [ForkJoinPool-2-worker-3] INFO  AccessLog$ - 200 GET /top20chart?section=business in 236.6 ms
+  val accessCompletedLine: P[AccessCompleted] = P(intDigits ~ " GET " ~ (noSpace.rep(min=1)!) ~ " in " ~ StopwatchTimeParsing.stopwatchDuration ).map(AccessCompleted.tupled)
+
+  val recognisedLines = P((logLineStart ~ elasticSearchCompletedLine) | (logLineStart.filter(_.logger=="AccessLog$") ~ accessCompletedLine))
 
   def parse(line: String, lineNumber: Int): SignificantInterval = {
     println(s"looking at : '$line'")
 
-    elasticSearchCompletedLine.parse(line).fold[SignificantInterval]((_,_,_) => null, {
-      case ((lls, query, elapsedDuration, elasticSearchDuration), i) =>
-        println(s"got $lls")
+    recognisedLines.parse(line).fold[SignificantInterval]((_,_,_) => null, {
+      case ((lls, payload), i) =>
+        println(s"got $lls $payload")
+
 
         val logInstantAtEnd = new LogInstant(lls.instant.toEpochMilli, lineNumber)
         val threadModel = threadedSystem.getOrCreateThread(lls.threadName)
 
-        val logInterval = new LogInterval(org.joda.time.Duration.millis(elapsedDuration.toMillis), logInstantAtEnd)
-        val significantInterval = new SignificantInterval(Map("type" -> "ES Request", "Query" -> query).asJava,
-          logInterval)
+        val logInterval = new LogInterval(org.joda.time.Duration.millis(payload.elapsedDuration.toMillis), logInstantAtEnd)
+
+        val siPayload: Map[String, String] = payload match {
+          case es: ESCompleted => Map("type" -> "ES Request", "Query" -> es.query)
+          case ac: AccessCompleted =>Map("type" -> "Page Request", "Path" -> ac.path)
+        }
+
+        val significantInterval = new SignificantInterval(siPayload.asJava, logInterval)
         threadModel.add(significantInterval)
         significantInterval
     }
--- glug-model/src/test/scala/glug/FooLogLineParserTest.scala
+++ glug-model/src/test/scala/glug/FooLogLineParserTest.scala
@@ -7,16 +7,29 @@ import scala.collection.mutable.Stack
 
 class FooLogLineParserTest extends FlatSpec with Matchers {
 
+  private val fooLogLineParser = new FooLogLineParser(new ThreadedSystem())
+
   "A line parser" should "understand a sample line" in {
     val sample = "2017-10-04 09:08:00,037 [application-akka.actor.default-dispatcher-745] INFO  lib.DashboardData$ - completed lib.DashboardData$::topPagesQueryNonContentAgg in 121 ms elapsed, 116 ms elasticsearch"
-    val boo = new FooLogLineParser(new ThreadedSystem()).elasticSearchCompletedLine.parse(sample)
+    val boo = fooLogLineParser.recognisedLines.parse(sample)
     println(boo.get.value)
   }
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a[NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
-    }
+  it should "parse an log line start" in {
+    val sample = "2017-10-04 09:08:00,121 [ForkJoinPool-2-worker-3] INFO  AccessLog$ - 200 GET /top20chart?section=business in 236.6 ms"
+    val boo = fooLogLineParser.logLineStart.parse(sample)
+    println(boo.get.value)
+  }
+
+  it should "parse an access log line" in {
+    val sample = "2017-10-04 09:08:00,121 [ForkJoinPool-2-worker-3] INFO  AccessLog$ - 200 GET /top20chart?section=business in 236.6 ms"
+    val boo = fooLogLineParser.recognisedLines.parse(sample)
+    println(boo.get.value)
+  }
+
+  it should "parse the payload of an access log line" in {
+    val sample = "200 GET /top20chart?section=business in 236.6 ms"
+    val boo = fooLogLineParser.accessCompletedLine.parse(sample)
+    println(boo.get.value)
   }
 }
